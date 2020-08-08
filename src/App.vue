8af52c59d5c03acc0968ca7f37d251fc3d4b546e
Bolat Kazybayev <b.kazybaev@gmail.com>
2020-08-08 10:15:16 +0600

    [feat] Add Navigation Bar

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,20 @@
 <template>
   <v-app>
-    <v-content class="ma-4">
+    <NavBar/>
+    <v-main class="ma-4">
       <router-view></router-view>
-    </v-content>
+    </v-main>
   </v-app>
 </template>
+
+<script>
+import NavBar from '@/components/NavBar'
+
+export default {
+  name: 'App',
+  components: {
+    NavBar
+  }
+    
+}
+</script>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,29 @@
+<template>
+  <nav>
+    <v-app-bar color="red" dark app>
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-toolbar-title class="text-uppercase">
+        <span class="font-weight-light">Akzere</span>
+        <span> Big Byte</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn text>
+        <span>Exit</span>
+        <v-icon right>exit_to_app</v-icon>
+      </v-btn>
+    </v-app-bar>
+  </nav>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    drawer: true,
+    links: [
+      { icon: "dashboard", text: "dashboard", route: "/" },
+      { icon: "folder", text: "My Project", route: "/projects" },
+      { icon: "person", text: "Team", route: "/team" },
+    ],
+  }),
+};
+</script>
