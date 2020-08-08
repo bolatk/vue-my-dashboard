e59d04a07d86e7808197fee53e67ce62e4979fbf
Bolat Kazybayev <b.kazybaev@gmail.com>
2020-08-08 10:31:43 +0600

    [feat] Add Menu and Navigation Drawer

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,17 +1,54 @@
 <template>
   <nav>
     <v-app-bar color="red" dark app>
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
       <v-toolbar-title class="text-uppercase">
         <span class="font-weight-light">Akzere</span>
-        <span> Big Byte</span>
+        <span>Big Byte</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
+      <v-menu offset-y>
+        <template v-slot:activator="{ on }">
+          <v-btn text v-on="on">
+            <v-icon left>expand_more</v-icon>
+            <span>Menu</span>
+          </v-btn>
+        </template>
+        <v-list flat>
+          <v-list-item
+            v-for="link in links"
+            :key="link.text"
+            router
+            :to="link.route"
+            active-class="border"
+          >
+            <v-list-item-title>{{link.text}}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
       <v-btn text>
         <span>Exit</span>
         <v-icon right>exit_to_app</v-icon>
       </v-btn>
     </v-app-bar>
+    <v-navigation-drawer v-model="drawer" dark app class="red darken-4">
+      <v-list flat>
+        <v-list-item
+          v-for="link in links"
+          :key="link.text"
+          router
+          :to="link.route"
+          active-class="border"
+        >
+          <v-list-item-action>
+            <v-icon>{{link.icon}}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>{{link.text}}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
   </nav>
 </template>
 
@@ -20,10 +57,16 @@ export default {
   data: () => ({
     drawer: true,
     links: [
-      { icon: "dashboard", text: "dashboard", route: "/" },
+      { icon: "dashboard", text: "Dashboard", route: "/" },
       { icon: "folder", text: "My Project", route: "/projects" },
       { icon: "person", text: "Team", route: "/team" },
     ],
   }),
 };
 </script>
+
+<style scoped>
+.border {
+  border-left: 4px solid #0ba518;
+}
+</style>
